699e02f661d4699bd3fdd16b9e36e793f455154a
tapia-ops <248371069+tapia-ops@users.noreply.github.com>
2026-05-21 01:47:29 +0800

    Add sumNUmbers method

--- MethodDemo.java
+++ MethodDemo.java
@@ -3,9 +3,17 @@ public class MethodDemo {
         System.out.println("Welcome to the Method Demo!");
         
         printGreetring("Alice");
+        printGreetring("Bob");
+
+        int result = sumNumbers(5, 7);
+        System.out.println("The sum of 5 and 7 is: " + result);
     }
     
     public static void printGreetring(String name) {
         System.out.println("Hello, " + name + "! Welcome to Java methods");
+    }
+
+    private static int sumNumbers(int a, int b) { // Add a second method to sum integers
+        return a + b;
     }
 }
